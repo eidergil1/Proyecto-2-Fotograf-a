db9c444f8bf9f73387bbd777a04ef4ec71b01b04
eidergil1 <eidergil@outlook.com>
2024-01-16 20:29:24 +0100

    clase1601

--- php/enviar.php
+++ php/enviar.php
@@ -1,34 +1,46 @@
 <?php
-ini_set('display_errors', 1);
 
-/* recoger datos y comprobar */
-$destino = "mirianamaya@gmail.com";/* correo del admin */
-$nombre = $_POST["nombre"];/* datos del usuario */
-$correo = $_POST["correo"];
-$telefono = $_POST["telefono"];
-$mensaje = $_POST["mensaje"];
 
-/* datos que cogemos de su CLIENTE */
-/* ip */
-$ip = $_SERVER['REMOTE_ADDR'];
+/* 1 - INSTANCIAMOS RECURSOS Y CLASES REQUERIMOS DOS ARCHIVOS PHP (uso de clases y objetos) */
+require_once "./includes/_config.php";
 
-/* datos que cogemos deL SISTEMA */
-/* fecha */
-$datetime = date("Y-m-d H:i:s");
 
-/* mensaje concatenado para enviar por correo*/
-$contenido = "fecha de envío: ".$datetime."\nIP: ".$ip."\nNombre: ".$nombre."\nCorreo: ".$correo."\nTelefono: ".$telefono."\nMensaje: ".$mensaje;
+/* 2 - COMPROBAMOS QUE VENGAMOS POR %_POST 
+Hacemos una condición para ver si existe $_POST */
+if(isset($_POST)){
 
-$cabecera = 'From mirianamaya@gmail.com'."\r\n".
-'Reply-To: eidergil@outlook.com'."\r\n".
-'X-Mailer: PHP/'.phpversion();
+    /* 3 - RECOGEMOS EN VARIABLES LOS VALORES DE LOS IMPUT DEL FORM */
+    $destino = "eidergil@outlook.com";/* correo del admin */
+    $nombre = $_POST["nombre"];/* datos del usuario */
+    $correo = $_POST["correo"];
+    $telefono = $_POST["telefono"];
+    $mensaje = $_POST["mensaje"];
 
-/* 9-enviar correos de confirmacion*/
-mail($destino,"Consulta de la web", $contenido,$cabecera); /* correo que recibo yo */
+   /* comprobaciones */
 
-mail($correo,"Hemos recibiido tu consulta",$contenido,$cabecera); /* correo que recibe el cliente */
+    /* datos que cogemos de su CLIENTE */
+    /* ip */
+    $ip = $_SERVER['REMOTE_ADDR'];
 
-/* 10-redirigir a gracias*/
-header("location:../index.html?enviado=correo enviado!");
+    /* datos que cogemos deL SISTEMA */
+    /* fecha */
+    $datetime = date("Y-m-d H:i:s");
+
+    /* mensaje concatenado para enviar por correo*/
+    $contenido = "fecha de envío: ".$datetime."\n\rIP: ".$ip."\n\rNombre: ".$nombre."\n\rCorreo: ".$correo."\n\rTelefono: ".$telefono."\n\rMensaje: ".$mensaje;
+
+    $cabecera = 'From info@webda.eus'."\r\n".
+    /* 'Reply-To: info@webda.eus'."\r\n". */
+    'X-Mailer: PHP/'.phpversion();
+
+    /* 9-enviar correos de confirmacion*/
+    mail($destino,"Consulta de la web", $contenido,$cabecera); /* correo que recibo yo */
+
+    mail($correo,"Hemos recibicido tu consulta",$contenido,$cabecera); /* correo que recibe el cliente */
+
+    /* 10-redirigir a gracias*/
+    header("location:../index.html?enviado=correo enviado!");
+
+}
 ?>
 
---
+++ php/includes/_config.php
@@ -0,0 +1,6 @@
+<?php
+/* si es 0 es false, si es 1 es true
+mostramos errores en PHP */
+ini_set("display_errors",1)
+
+?>
